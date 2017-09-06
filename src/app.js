2d32bf49bb683cefa99278159605e1608e85990d
Jenia Brook <jeniabrook@gmail.com>
2017-09-07 00:11:44 +0300

    Added middleware for redux-logger

--- src/app.js
+++ src/app.js
@@ -1,15 +1,13 @@
 "use strict"
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
+import logger from 'redux-logger';
 import reducers from './reducers/index';
 import {addToCart} from './actions/cartActions';
 import {deleteBooks, postBooks, updateBooks} from './actions/booksActions';
 
 // create a store
-const store = createStore(reducers);
-
-store.subscribe(() => {
-  console.log('Current state is: ',  store.getState());
-});
+const middleware = applyMiddleware(logger)
+const store = createStore(reducers, middleware);
 
 // create and dispatch actions
 store.dispatch(postBooks(
